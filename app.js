bbfca40d244eb19a50bc1583fa0c9b8874a58ebf
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2018-12-11 15:01:34 +0200

    adding socket.io to server and test connection

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ var app = express();
 
 //---------------------- middleware----------------
 app.use(express.static('public'));
+// Integrating Socket.IO server
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 
 
 //------------------------ server init ------------------
-app.listen(port,()=>{
+http.listen(port,()=>{
 	console.log(`server on ${port}`);
 });
